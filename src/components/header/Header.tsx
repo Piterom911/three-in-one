6f8f74124452890935b8b8d80467aaf900c0584f
Roman Dogoda <piterom911@gmail.com>
2023-11-13 20:16:32 +0100

    add some new styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -45,7 +45,7 @@ function Header() {
           >
             <path
               d="M37.5896 0.25L74.5396 64.25H0.639648L37.5896 0.25Z"
-              fill="var(--geist-foreground)"
+              fill="#ffffff"
             ></path>
           </svg>
           <span>MyApp</span>
