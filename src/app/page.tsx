d0f746ea969ae2db427796498081d551a2fbdfa9
Roman Dogoda <piterom911@gmail.com>
2023-11-20 20:33:02 +0100

    Add image and styles to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
-  return <h1>Home </h1>;
+  return (
+    <div className={styles.content}>
+      <div className={styles.textWrapper}>
+        <h1 className={styles.heading}>Exploring the Power of Next.js</h1>
+        <p className={styles.subtitle}>
+          Unlock the Potential of Next.js: A Comprehensive Platform Tailored for
+          Seamless Learning and Mastery. Dive into Next.js with Confidence,
+          Supported by In-Depth Resources and Interactive Tutorials
+        </p>
+      </div>
+      <Image
+        className={styles.image}
+        src="/bg1.png"
+        alt="background home page"
+        width={400}
+        height={400}
+      />
+    </div>
+  );
 }
